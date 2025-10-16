8a734b4482b384b7abb398b91600b23966434bdc
[dyad] <projects.hyper@outlook.com>
2025-10-17 00:53:51 +0100

    [dyad] Removed the main container div from the homepage. - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 
 const Index = () => {
   return (
-    <div className="relative h-full bg-gradient-to-br from-gray-900 to-black text-white rounded-xl p-8">
-      {/* Images Section */}
-      <div className="absolute top-24 right-8 flex flex-col gap-4">
-        <img
-          src="/images/Group 1000005755.png"
-          alt="Leaderboard"
-          className="w-64 rounded-xl"
-        />
-        <img
-          src="/images/Group 1000005762.png"
-          alt="Prediction Card"
-          className="w-64 rounded-xl"
-        />
-      </div>
+    <div className="absolute top-24 right-8 flex flex-col gap-4">
+      <img
+        src="/images/Group 1000005755.png"
+        alt="Leaderboard"
+        className="w-64 rounded-xl"
+      />
+      <img
+        src="/images/Group 1000005762.png"
+        alt="Prediction Card"
+        className="w-64 rounded-xl"
+      />
     </div>
   );
 };
