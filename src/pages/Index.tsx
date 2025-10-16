31177af1f4cbbedc87747688e60ee9fd474ebd44
[dyad] <projects.hyper@outlook.com>
2025-10-17 00:55:48 +0100

    [dyad] Expanding the main div to fill the desktop view - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,19 @@
+"use client";
+
 import React from 'react';
 
 const Index = () => {
   return (
-    <div className="absolute top-24 right-8 flex flex-col gap-4">
+    <div className="w-full h-full flex flex-col items-center justify-center gap-4 p-4">
       <img
         src="/images/Group 1000005755.png"
         alt="Leaderboard"
-        className="w-64 rounded-xl"
+        className="max-w-full h-auto"
       />
       <img
-        src="/images/Group 1000005762.png"
+        src="/images/Group 1000005756.png"
         alt="Prediction Card"
-        className="w-64 rounded-xl"
+        className="max-w-full h-auto"
       />
     </div>
   );
