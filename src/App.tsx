1fd2e7142666273df37c7769b24fafa298760742
[dyad] <projects.hyper@outlook.com>
2025-10-17 00:38:14 +0100

    [dyad] Removed padding from the main content area. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,22 @@
 "use client";
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+import { Routes, Route } from 'react-router-dom';
 import MainHeader from './components/MainHeader';
 import Index from './pages/Index';
 import Games from './pages/Games';
-import Pools from './pages/Pools';
-import Leaderboard from './pages/Leaderboard';
-import Wallet from './pages/Wallet';
-import NotFound from './pages/NotFound';
-import Terms from './pages/Terms';
-import Help from './pages/Help';
-import Contact from './pages/Contact';
-import Users from './pages/Users';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import Support from './pages/Support';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="flex min-h-screen bg-vanta-blue-dark text-vanta-text-light">
-        <Sidebar />
-        <div className="flex-1 ml-[19rem] mt-4 mr-4 mb-4 rounded-xl overflow-hidden">
-          <MainHeader />
-          <div className="p-4">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/games" element={<Games />} />
-              <Route path="/pools" element={<Pools />} />
-              <Route path="/leaderboard" element={<Leaderboard />} />
-              <Route path="/wallet" element={<Wallet />} />
-              {/* Removed Rewards and UserSettings routes */}
-              <Route path="/users" element={<Users />} />
-              <Route path="/terms-of-use" element={<Terms />} />
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route path="/help" element={<Help />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/support" element={<Support />} />
-              <Route path="/sports/:category" element={<div>Sport Category Page</div>} />
-              <Route path="/how-it-works" element={<div>How It Works Page</div>} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-950 text-white">
+      <MainHeader />
+      {/* Removed the div with p-4 padding */}
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/games" element={<Games />} />
+      </Routes>
+    </div>
   );
 }
 
