45f1104f7dd43622498d576a66ea72964828e385
[dyad] <projects.hyper@outlook.com>
2025-10-17 01:50:46 +0100

    [dyad] Removed the 'Start Playing' button from the Index page. - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,13 +6,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 const Index = () => {
   return (
     <div className="relative h-full bg-gradient-to-br from-gray-900 to-black text-white rounded-xl p-8">
-      <div className="mt-8 text-center">
-        <Link to="/game">
-          <Button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg text-lg">
-            Start Playing
-          </Button>
-        </Link>
-      </div>
+      {/* The Index page is now empty */}
     </div>
   );
 };
