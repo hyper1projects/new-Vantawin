23dd6d158a923b9313064cac19d75a536b08df8e
[dyad] <projects.hyper@outlook.com>
2025-10-17 01:03:56 +0100

    [dyad] Adding and positioning the Leaderboard card on the homepage - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import React from 'react';
-// Link is no longer needed as there's no clickable content
-// import { Link } from 'react-router-dom'; 
+import LeaderboardCard from '../components/LeaderboardCard'; // Import the new component
 
 const Index = () => {
   return (
-    <div className="relative w-full h-full flex flex-col items-center justify-center gap-4 p-4">
-      {/* All previous content (leaderboard link and prediction card) has been removed */}
-      <p className="text-lg text-gray-500">Welcome to the homepage!</p>
-      <p className="text-sm text-gray-400">Content will be added here.</p>
+    <div className="relative w-full h-full min-h-[calc(100vh-10rem)]"> {/* Ensure enough height for positioning */}
+      <div className="absolute top-4 right-4"> {/* Position the card */}
+        <LeaderboardCard />
+      </div>
+      {/* The rest of your homepage content will go here, below the header and to the left of the card */}
+      <div className="flex flex-col items-center justify-center gap-4 pt-28"> {/* Placeholder for other content */}
+        <p className="text-lg text-gray-500">Welcome to the homepage!</p>
+        <p className="text-sm text-gray-400">Content will be added here.</p>
+      </div>
     </div>
   );
 };
