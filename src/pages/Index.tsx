1e3cb3cd1dbc6e61850f21cc6a8bd70b863b8b33
[dyad] <projects.hyper@outlook.com>
2025-10-21 00:10:33 +0100

    [dyad] Changed heading font to Outfit - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ImageCarousel from '../components/ImageCarousel';
-import PointsMultiplierSection from '../components/PointsMultiplierSection'; // Import the new component
+import PointsMultiplierSection from '../components/PointsMultiplierSection';
 
 const Index = () => {
   return (
     <div className="p-4">
-      <h1 className="text-xl font-bold text-vanta-text-light mb-4 text-left pl-4">Welcome to VantaWin!</h1>
+      <h1 className="text-xl font-bold text-vanta-text-light mb-4 text-left pl-4 font-outfit">Welcome to VantaWin!</h1>
       <ImageCarousel />
       {/* Add the new PointsMultiplierSection component here */}
       <PointsMultiplierSection />
