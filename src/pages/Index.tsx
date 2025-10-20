1e6adeb382dd75d5ece1e02fc5442b668f508498
[dyad] <projects.hyper@outlook.com>
2025-10-21 00:07:02 +0100

    [dyad] Added Points Multiplier section - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ImageCarousel from '../components/ImageCarousel';
+import PointsMultiplierCard from '../components/PointsMultiplierCard'; // Import the new component
 
 const Index = () => {
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold text-vanta-text-light mb-4 text-left pl-4">Welcome to VantaWin!</h1>
       <ImageCarousel />
-      {/* You can add more content here later */}
+      {/* Add the new PointsMultiplierCard component here */}
+      <PointsMultiplierCard />
       <div className="mt-8 text-center text-vanta-text-light">
         <p>Explore the features of your new VantaWin application!</p>
       </div>
