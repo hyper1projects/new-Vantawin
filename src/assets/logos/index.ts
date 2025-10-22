e7b577f84dab5b558b172445caef2c62a402aa46
[dyad] <projects.hyper@outlook.com>
2025-10-22 09:24:57 +0100

    [dyad] wrote 1 file(s)

--- src/assets/logos/index.ts
+++ src/assets/logos/index.ts
@@ -2,7 +2,7 @@
 
 // Import the specific image files using relative paths from this file.
 import manUnited from './England - Premier League/Manchester United.png';
-import leicesterCity from './England - Premier League/Leicester City.png';
+import leicesterCity from '../images/leicester_city_logo.png'; // Corrected path for Leicester City
 import crystalPalace from './England - Premier League/Crystal Palace.png';
 import westHamUnited from './England - Premier League/West Ham United.png';
 import astonVilla from './England - Premier League/Aston Villa.png';
