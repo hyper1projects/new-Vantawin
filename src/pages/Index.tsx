851a377a5d7ffd084d3995b58a8b4fb3e8dcf77e
[dyad] <projects.hyper@outlook.com>
2025-10-23 22:02:10 +0100

    Reverted all changes back to version 7486efdc0d7126cc96f4400f2192000706c9aa6e

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import React from 'react';
-// import Oddscard from '../components/Oddscard'; // Temporarily commented out to isolate the issue
+import ImageCarousel from '../components/ImageCarousel';
+import PointsMultiplierSection from '../components/PointsMultiplierSection';
+import TopGamesSection from '../components/TopGamesSection'; // Import the new component
 
-const Index: React.FC = () => {
+const Index = () => {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-6 text-gray-800">Hello from Index Page!</h1>
+      <h1 className="text-xl font-bold text-vanta-text-light mb-4 text-left pl-4">Welcome to VantaWin!</h1>
+      <ImageCarousel />
+      <PointsMultiplierSection />
+      {/* Add the new TopGamesSection component here */}
+      <TopGamesSection />
+      <div className="mt-8 text-center text-vanta-text-light">
+        <p>Explore the features of your new VantaWin application!</p>
+      </div>
     </div>
   );
 };
