d4960ca2bbfce529b2d3e3883ac6bef4d40f5d0c
[dyad] <projects.hyper@outlook.com>
2025-10-23 08:08:58 +0100

    [dyad] Fixed import error in PointsMultiplierSection - wrote 1 file(s)

--- src/components/PointsMultiplierSection.tsx
+++ src/components/PointsMultiplierSection.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import React from 'react';
-import MatchCard from './MatchCard'; // Assuming MatchCard is in the same directory or correctly imported
+import Oddscard from './Oddscard'; // Changed from MatchCard to Oddscard
+import SectionHeader from './SectionHeader';
 import { TeamLogos } from '@/assets/logos'; // Import the centralized TeamLogos map
 
 const PointsMultiplierSection: React.FC = () => {
@@ -9,32 +10,50 @@ const PointsMultiplierSection: React.FC = () => {
     <div className="w-full py-8 px-4">
       <h2 className="text-xl md:text-2xl font-bold text-white mb-8 text-left tracking-tight">Points Multiplier</h2>
       <div className="flex flex-col md:flex-row justify-center items-center md:items-stretch gap-6">
-        <MatchCard
-          date="Today"
-          time="09:00 PM"
+        <Oddscard
+          matchDate="Today"
+          matchTime="19:00"
+          isLive={false}
           team1Logo={TeamLogos.MANU}
           team1Name="Man. United"
           team2Logo={TeamLogos.LEIC}
           team2Name="Leicester City"
-          multiplier="2.5x"
+          option1Label="1"
+          option1Value="1.90"
+          option2Label="X"
+          option2Value="3.10"
+          option3Label="2"
+          option3Value="2.40"
         />
-        <MatchCard
-          date="Tomorrow"
-          time="09:00 PM"
-          team1Logo={TeamLogos.MANU}
-          team1Name="Man. United"
-          team2Logo={TeamLogos.LEIC}
-          team2Name="Leicester City"
-          multiplier="2.5x"
+        <Oddscard
+          matchDate="Tomorrow"
+          matchTime="21:00"
+          isLive={true}
+          team1Logo={TeamLogos.CRY}
+          team1Name="Crystal Palace"
+          team2Logo={TeamLogos.ASTON}
+          team2Name="Aston Villa"
+          option1Label="1"
+          option1Value="2.20"
+          option2Label="X"
+          option2Value="3.00"
+          option3Label="2"
+          option3Value="2.10"
         />
-        <MatchCard
-          date="Tomorrow"
-          time="09:00 PM"
-          team1Logo={TeamLogos.MANU}
-          team1Name="Man. United"
-          team2Logo={TeamLogos.LEIC}
-          team2Name="Leicester City"
-          multiplier="2.5x"
+        <Oddscard
+          matchDate="Upcoming"
+          matchTime="16:30"
+          isLive={false}
+          team1Logo={TeamLogos.WHU}
+          team1Name="West Ham United"
+          team2Logo={TeamLogos.MANU}
+          team2Name="Man. United"
+          option1Label="1"
+          option1Value="2.00"
+          option2Label="X"
+          option2Value="3.25"
+          option3Label="2"
+          option3Value="2.30"
         />
       </div>
     </div>
