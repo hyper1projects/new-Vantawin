82aff8ab4f6995d4de19905451c6ec07e917fc73
[dyad] <projects.hyper@outlook.com>
2025-10-25 09:13:02 +0100

    [dyad] Ensured consistent spacing for main sections on the index page. - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,16 +3,15 @@
 import React from 'react';
 import ImageCarousel from '../components/ImageCarousel';
 import PointsMultiplierSection from '../components/PointsMultiplierSection';
-import TopGamesSection from '../components/TopGamesSection'; // Import the new component
+import TopGamesSection from '../components/TopGamesSection';
 
 const Index = () => {
   return (
-    <div className="p-4">
+    <div className="p-16">
       <h1 className="text-xl font-bold text-vanta-text-light mb-4 text-left pl-4">Welcome to VantaWin!</h1>
-      <ImageCarousel className="mb-8" /> {/* Added mb-8 here */}
-      <PointsMultiplierSection className="mb-8" /> {/* Added mb-8 here */}
-      {/* Add the new TopGamesSection component here */}
-      <TopGamesSection />
+      <ImageCarousel className="mb-8" />
+      <PointsMultiplierSection className="mb-8" />
+      <TopGamesSection className="mb-8" /> {/* Added mb-8 here for consistency */}
       <div className="mt-8 text-center text-vanta-text-light">
         <p>Explore the features of your new VantaWin application!</p>
       </div>
