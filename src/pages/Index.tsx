7cb72b61569706fd769e5d7ff6a6f6b828ed9496
[dyad] <projects.hyper@outlook.com>
2025-10-25 09:36:08 +0100

    [dyad] Added bottom spacing to ImageCarousel, PointsMultiplierSection, and TopGamesSection in Index.tsx. - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,14 +4,15 @@ import React from 'react';
 import ImageCarousel from '../components/ImageCarousel';
 import PointsMultiplierSection from '../components/PointsMultiplierSection';
 import TopGamesSection from '../components/TopGamesSection'; // Import the new component
+
 const Index = () => {
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold text-vanta-text-light mb-4 text-left pl-4">Welcome to VantaWin!</h1>
-      <ImageCarousel />
-      <PointsMultiplierSection />
+      <ImageCarousel className="mb-8" />
+      <PointsMultiplierSection className="mb-8" />
       {/* Add the new TopGamesSection component here */}
-      <TopGamesSection />
+      <TopGamesSection className="mb-8" />
       <div className="mt-8 text-center text-vanta-text-light">
         <p>Explore the features of your new VantaWin application!</p>
       </div>
