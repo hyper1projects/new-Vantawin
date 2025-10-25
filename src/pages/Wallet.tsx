900e07cd6a3adae5c2fbd3877ddd52d4fc886b11
[dyad] <projects.hyper@outlook.com>
2025-10-25 17:08:37 +0100

    [dyad] Arranged WalletOverviewCard and ProfitLossCard side-by-side - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -2,13 +2,19 @@
 
 import React from 'react';
 import WalletOverviewCard from '../components/WalletOverviewCard';
-import ProfitLossCard from '../components/ProfitLossCard'; // Import the new component
+import ProfitLossCard from '../components/ProfitLossCard';
 
 const Wallet = () => {
   return (
-    <div className="p-4 space-y-6"> {/* Added space-y-6 for spacing between cards */}
-      <WalletOverviewCard />
-      <ProfitLossCard /> {/* Render the new ProfitLossCard component */}
+    <div className="p-4">
+      <div className="flex space-x-6"> {/* Flex container for side-by-side layout */}
+        <div className="flex-1"> {/* WalletOverviewCard takes half width */}
+          <WalletOverviewCard />
+        </div>
+        <div className="flex-1"> {/* ProfitLossCard takes half width */}
+          <ProfitLossCard />
+        </div>
+      </div>
     </div>
   );
 };
