e3590c1d550b5d90f48f0f2a927b9e6b442303f3
[dyad] <projects.hyper@outlook.com>
2025-10-25 13:43:05 +0100

    Updated src/components/MainHeader.tsx

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
 const MainHeader: React.FC = () => {
-  const sportsCategories = ['Football', 'Basketball', 'Tennis', 'A.Football', 'Golf'];
+  const sportsCategories = ['Football', 'Basketball', 'Tennis', 'Esports',];
   const location = useLocation();
   const currentPath = location.pathname;
 
