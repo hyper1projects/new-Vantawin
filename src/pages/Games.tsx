1cf006499130e2dce26162195e9d7d56f8e93bdd
[dyad] <projects.hyper@outlook.com>
2025-10-25 10:46:49 +0100

    [dyad] Added the LiveGamesSection component to the Games page. - wrote 1 file(s)

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import React from 'react';
+import LiveGamesSection from '../components/LiveGamesSection'; // Import LiveGamesSection
 
 const Games: React.FC = () => {
   return (
-    <div className="p-4">
+    <div className="container mx-auto p-4 bg-vanta-base min-h-screen">
       <h1 className="text-2xl font-bold text-vanta-text-light mb-4">Games</h1>
-      <p className="text-vanta-text-medium">Explore various games here!</p>
+      <p className="text-vanta-text-medium mb-6">Explore various games here!</p>
+      
+      {/* Add the LiveGamesSection here */}
+      <LiveGamesSection className="mb-8" />
+      
+      {/* You can add other game sections or content below */}
     </div>
   );
 };
