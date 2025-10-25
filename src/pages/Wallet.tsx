615916c7a5ec71a974402831ce48bce953f4a301
[dyad] <projects.hyper@outlook.com>
2025-10-25 17:06:31 +0100

    [dyad] Added a new ProfitLossCard component to the Wallet page - wrote 2 file(s) + extra files edited outside of Dyad

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import React from 'react';
-// Removed SectionHeader import as it's no longer used
 import WalletOverviewCard from '../components/WalletOverviewCard';
+import ProfitLossCard from '../components/ProfitLossCard'; // Import the new component
 
 const Wallet = () => {
   return (
-    <div className="p-4">
-      {/* Removed SectionHeader component */}
-      {/* Removed "Manage your funds here!" text */}
-      
-      {/* Render the WalletOverviewCard component */}
+    <div className="p-4 space-y-6"> {/* Added space-y-6 for spacing between cards */}
       <WalletOverviewCard />
+      <ProfitLossCard /> {/* Render the new ProfitLossCard component */}
     </div>
   );
 };
