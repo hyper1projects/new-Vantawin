aa0e4e7883f3c555171fab361b9dd69e51a777b6
[dyad] <projects.hyper@outlook.com>
2025-10-25 09:02:15 +0100

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,6 +11,7 @@ const Index = () => {
       <h1 className="text-xl font-bold text-vanta-text-light mb-4 text-left pl-4">Welcome to VantaWin!</h1>
       <ImageCarousel />
       <PointsMultiplierSection />
+      <div className="h-48 bg-gray-800 rounded-lg flex items-center justify-center mb-16">
       {/* Add the new TopGamesSection component here */}
       <TopGamesSection />
       <div className="mt-8 text-center text-vanta-text-light">
