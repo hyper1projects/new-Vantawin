2f4dd5db11be332c542f362e3afabc3a85b0b003
[dyad] <projects.hyper@outlook.com>
2025-10-30 16:47:23 +0100

    [dyad] Added game details to MatchHeaderImage. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/MatchHeaderImage.tsx
+++ src/components/MatchHeaderImage.tsx
@@ -2,34 +2,40 @@
 
 import React from 'react';
 import { Game } from '../types/game';
-import { getLogoSrc } from '../utils/logoMap';
+import { getLogoSrc } from '../utils/teamLogos';
 
 interface MatchHeaderImageProps {
   game: Game;
 }
 
 const MatchHeaderImage: React.FC<MatchHeaderImageProps> = ({ game }) => {
+  const { team1, team2, date, time, league } = game;
+
+  // Format date and time
+  const formattedDateTime = `${date} - ${time}`;
+
   return (
-    <div 
+    <div
       className="relative w-full h-[200px] bg-cover bg-center rounded-[27px] mb-8 flex items-center justify-between px-8"
       style={{ backgroundImage: 'url(/images/Group 1000005769.png)' }}
     >
-      {/* Team 1 Info */}
-      <div className="flex flex-col items-center text-center w-1/3">
-        <img src={getLogoSrc(game.team1.logoIdentifier)} alt={game.team1.name} className="w-20 h-20 object-contain mb-2" />
-        <span className="text-xl font-bold text-vanta-text-light">{game.team1.name}</span>
+      {/* Team 1 */}
+      <div className="flex flex-col items-center space-y-2">
+        <img src={getLogoSrc(team1.name)} alt={team1.name} className="w-16 h-16 object-contain" />
+        <span className="text-white text-lg font-bold">{team1.name}</span>
       </div>
 
-      {/* Center Info (Time only) */}
-      <div className="flex flex-col items-center text-center w-1/3">
-        <span className="text-2xl font-bold text-vanta-text-light mb-2">{game.time}</span>
-        {/* Removed league logo and name */}
+      {/* Central Details: League, Date/Time, VS */}
+      <div className="flex flex-col items-center justify-center space-y-1">
+        <span className="text-vanta-text-light text-sm font-medium">{league}</span>
+        <span className="text-vanta-text-light text-xs">{formattedDateTime}</span>
+        <span className="text-vanta-neon-blue text-3xl font-bold">VS</span>
       </div>
 
-      {/* Team 2 Info */}
-      <div className="flex flex-col items-center text-center w-1/3">
-        <img src={getLogoSrc(game.team2.logoIdentifier)} alt={game.team2.name} className="w-20 h-20 object-contain mb-2" />
-        <span className="text-xl font-bold text-vanta-text-light">{game.team2.name}</span>
+      {/* Team 2 */}
+      <div className="flex flex-col items-center space-y-2">
+        <img src={getLogoSrc(team2.name)} alt={team2.name} className="w-16 h-16 object-contain" />
+        <span className="text-white text-lg font-bold">{team2.name}</span>
       </div>
     </div>
   );
