1242ec14788929ea1f09b9017a93e98afa0ab8a6
[dyad] <projects.hyper@outlook.com>
2025-10-30 14:15:33 +0100

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/TotalGoalsCard.tsx
+++ src/components/TotalGoalsCard.tsx
@@ -4,14 +4,13 @@ import React from 'react';
 import { Game } from '../types/game';
 import OddsButton from './OddsButton';
 import { useMatchSelection } from '../context/MatchSelectionContext';
-import { getLogoSrc } from '../utils/logoMap';
+// Removed getLogoSrc import as logos are no longer used
 
 interface TotalGoalsCardProps {
   game: Game;
 }
 
 const TotalGoalsCard: React.FC<TotalGoalsCardProps> = ({ game }) => {
-  const { team1, team2 } = game;
   const { selectedGame, selectedOutcome, setSelectedMatch } = useMatchSelection();
 
   const handleOddsClick = (e: React.MouseEvent, outcome: 'team1' | 'draw' | 'team2') => {
@@ -25,7 +24,7 @@ const TotalGoalsCard: React.FC<TotalGoalsCardProps> = ({ game }) => {
       case 'score_goals':
         return `Will ${game.team1.name} score more than 2 goals?`;
       case 'over_2_5_goals':
-        return `Will there be 2 or more goals?`; // Changed text here
+        return `Will there be 2 or more goals?`;
       default:
         return `Total Goals question for ${game.team1.name} vs ${game.team2.name}`;
     }
@@ -43,31 +42,22 @@ const TotalGoalsCard: React.FC<TotalGoalsCardProps> = ({ game }) => {
         {getQuestionText(game)}
       </h3>
 
-      {/* Team Logos/Names and Buttons */}
+      {/* Buttons only, centered */}
       <div className="flex items-center justify-center space-x-6 w-full">
-        <div className="flex flex-col items-center">
-          <img src={getLogoSrc(team1.logoIdentifier)} alt={team1.name} className="w-16 h-16 object-contain mb-2" />
-          <span className="text-lg font-semibold">{team1.name}</span>
-          <OddsButton
-            value={game.odds.team1}
-            label="Yes"
-            onClick={(e) => handleOddsClick(e, 'team1')}
-            isSelected={selectedGame?.id === game.id && selectedOutcome === 'team1'}
-            className="rounded-[12px] px-6 py-2 mt-2"
-          />
-        </div>
-        <span className="text-2xl font-bold text-vanta-neon-blue">VS</span>
-        <div className="flex flex-col items-center">
-          <img src={getLogoSrc(team2.logoIdentifier)} alt={team2.name} className="w-16 h-16 object-contain mb-2" />
-          <span className="text-lg font-semibold">{team2.name}</span>
-          <OddsButton
-            value={game.odds.team2}
-            label="No"
-            onClick={(e) => handleOddsClick(e, 'team2')}
-            isSelected={selectedGame?.id === game.id && selectedOutcome === 'team2'}
-            className="rounded-[12px] px-6 py-2 mt-2"
-          />
-        </div>
+        <OddsButton
+          value={game.odds.team1}
+          label="Yes"
+          onClick={(e) => handleOddsClick(e, 'team1')}
+          isSelected={selectedGame?.id === game.id && selectedOutcome === 'team1'}
+          className="rounded-[12px] px-6 py-2 mt-2"
+        />
+        <OddsButton
+          value={game.odds.team2}
+          label="No"
+          onClick={(e) => handleOddsClick(e, 'team2')}
+          isSelected={selectedGame?.id === game.id && selectedOutcome === 'team2'}
+          className="rounded-[12px] px-6 py-2 mt-2"
+        />
       </div>
     </div>
   );
