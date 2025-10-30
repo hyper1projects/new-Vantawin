c53a2f4ff832c4f95ab900fbf506e3d43613118a
[dyad] <projects.hyper@outlook.com>
2025-10-30 04:14:33 +0100

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/GameDetails.tsx
+++ src/pages/GameDetails.tsx
@@ -37,7 +37,7 @@ const GameDetails: React.FC = () => {
 
       <div
         className="bg-vanta-blue-medium rounded-[27px] p-8 shadow-lg"
-        style={{ clipPath: 'polygon(0% 0%, 25% 0%, 35% 30px, 65% 30px, 75% 0%, 100% 0%, 100% 100%, 0% 100%)' }}
+        style={{ clipPath: 'polygon(0% 0%, 25% 0%, 30% 50px, 70% 50px, 75% 0%, 100% 0%, 100% 100%, 0% 100%)' }}
       >
         {/* Removed 'Game Overview' heading */}
 
