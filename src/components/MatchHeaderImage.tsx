583f3a136d650c0996be1e8fc4ea72e60ceb2b69
[dyad] <projects.hyper@outlook.com>
2025-10-30 04:26:59 +0100

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/MatchHeaderImage.tsx
+++ src/components/MatchHeaderImage.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import React from 'react';
-import { Game } from '../types/game';
-import { getLogoSrc } from '../utils/logoMap';
+import { Game } from '../data/games'; // Assuming Game type is defined here
 
 interface MatchHeaderImageProps {
   game: Game;
@@ -10,26 +9,32 @@ interface MatchHeaderImageProps {
 
 const MatchHeaderImage: React.FC<MatchHeaderImageProps> = ({ game }) => {
   return (
-    <div 
-      className="relative w-full h-[200px] bg-cover bg-center rounded-[27px] mb-8 flex items-center justify-between px-8"
-      style={{ backgroundImage: 'url(/images/Group 1000005769.png)' }}
-    >
-      {/* Team 1 Info */}
-      <div className="flex flex-col items-center text-center w-1/3">
-        <img src={getLogoSrc(game.team1.logoIdentifier)} alt={game.team1.name} className="w-20 h-20 object-contain mb-2" />
-        <span className="text-xl font-bold text-vanta-text-light">{game.team1.name}</span>
-      </div>
+    <div className="relative w-full h-64 rounded-[20px] overflow-hidden mb-6">
+      <img
+        src={game.image}
+        alt={`${game.team1.name} vs ${game.team2.name}`}
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="absolute inset-0 bg-gradient-to-t from-vanta-blue-dark via-transparent to-transparent opacity-90"></div>
 
-      {/* Center Info (Time only) */}
-      <div className="flex flex-col items-center text-center w-1/3">
-        <span className="text-2xl font-bold text-vanta-text-light mb-2">{game.time}</span>
-        {/* Removed league logo and name */}
-      </div>
+      <div className="absolute inset-0 flex items-start justify-between p-6"> {/* Changed items-center to items-start */}
+        {/* Team 1 */}
+        <div className="flex flex-col items-center text-center w-1/3">
+          <img src={game.team1.logo} alt={game.team1.name} className="w-16 h-16 mb-2" />
+          <span className="text-xl font-bold text-vanta-text-light">{game.team1.name}</span>
+        </div>
 
-      {/* Team 2 Info */}
-      <div className="flex flex-col items-center text-center w-1/3">
-        <img src={getLogoSrc(game.team2.logoIdentifier)} alt={game.team2.name} className="w-20 h-20 object-contain mb-2" />
-        <span className="text-xl font-bold text-vanta-text-light">{game.team2.name}</span>
+        {/* Match Info (Time) */}
+        <div className="flex flex-col items-center text-center w-1/3">
+          <span className="text-lg font-bold text-vanta-text-light">{game.time}</span> {/* Changed text-2xl to text-lg and removed mb-2 */}
+          {/* Removed league logo and name */}
+        </div>
+
+        {/* Team 2 */}
+        <div className="flex flex-col items-center text-center w-1/3">
+          <img src={game.team2.logo} alt={game.team2.name} className="w-16 h-16 mb-2" />
+          <span className="text-xl font-bold text-vanta-text-light">{game.team2.name}</span>
+        </div>
       </div>
     </div>
   );
