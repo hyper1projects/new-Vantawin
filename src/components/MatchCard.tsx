f44c1ba707e70c39e20daacc9cfe537a16c658cc
[dyad] <projects.hyper@outlook.com>
2025-10-31 07:11:13 +0100

    Reverted all changes back to version 50877cfd99fce36c1828dd609755ae68e008b2f5

--- src/components/MatchCard.tsx
+++ src/components/MatchCard.tsx
@@ -15,7 +15,10 @@ const MatchCard: React.FC<MatchCardProps> = ({ game }) => {
     <Link to={`/games/${game.id}`} className="relative p-[2px] rounded-[27px] bg-gradient-to-t from-[#9A3FFE] to-[#00EEEE] w-[230px] h-[280px] flex-shrink-0 cursor-pointer hover:scale-[1.02] transition-transform duration-200">
       <div className="bg-[#011B47] rounded-[27px] h-full w-full p-4 flex flex-col justify-between text-white">
         
-        {/* Team Logos and Names Row - MOVED TO TOP */}
+        {/* Date/Time Row */}
+        <p className="text-base font-semibold text-center mt-2">{game.date} - {game.time}</p> 
+        
+        {/* Team Logos and Names Row */}
         <div className="flex items-center justify-between w-full px-2"> 
           <div className="flex flex-col items-center w-1/3"> 
             <img src={getLogoSrc(game.team1.logoIdentifier)} alt={game.team1.name} className="w-12 h-12 object-contain mb-1" /> 
@@ -27,9 +30,8 @@ const MatchCard: React.FC<MatchCardProps> = ({ game }) => {
             <span className="text-[10px] font-medium text-center">{game.team2.name}</span>
           </div>
         </div>
-
-        {/* Date/Time Row - MOVED BELOW TEAM LOGOS */}
-        <p className="text-base font-semibold text-center mt-2">{game.date} - {game.time}</p> 
+        
+        {/* The odds display section (including the gradient border) has been completely removed */}
         
       </div>
     </Link>
