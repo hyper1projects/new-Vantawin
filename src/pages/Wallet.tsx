a967bbf691526a31c5b2d66de208443a5697cdb2
[dyad] <projects.hyper@outlook.com>
2025-11-11 22:38:38 +0100

    [dyad] Corrected Wallet page layout to display WalletTabs below other cards. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -7,22 +7,16 @@ import WalletTabs from '../components/WalletTabs';
 
 const Wallet = () => {
   return (
-    <div className="p-4 max-w-7xl mx-auto"> {/* Added max-w-7xl mx-auto for consistent page width */}
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        {/* Column 1: Wallet Overview Card */}
-        <div className="lg:col-span-1">
-          <WalletOverviewCard />
-        </div>
+    <div className="p-4 max-w-7xl mx-auto">
+      {/* Top section: Wallet Overview Card and Win/Loss Ratio Card */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6">
+        <WalletOverviewCard />
+        <WinLossRatioCard className="h-full" />
+      </div>
 
-        {/* Column 2 & 3: Win/Loss Ratio Card and Wallet Tabs */}
-        <div className="lg:col-span-2 flex flex-col lg:flex-row gap-6">
-          <div className="flex-1"> {/* Container for WinLossRatioCard */}
-            <WinLossRatioCard className="h-full" /> {/* Apply h-full to make it stretch */}
-          </div>
-          <div className="flex-1"> {/* Container for WalletTabs */}
-            <WalletTabs className="h-full" /> {/* Apply h-full to make it stretch */}
-          </div>
-        </div>
+      {/* Bottom section: Wallet Tabs (full width) */}
+      <div>
+        <WalletTabs className="h-full" />
       </div>
     </div>
   );
