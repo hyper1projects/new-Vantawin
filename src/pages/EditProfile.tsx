ae1489e9f4afdb5403f0d6eb0cc5cc18cffdf8a0
[dyad] <projects.hyper@outlook.com>
2025-11-12 14:49:31 +0100

    [dyad] Removed the global scrollbar by adjusting CSS on html/body and ensuring internal scrolling is handled by the layout. - wrote 2 file(s) + extra files edited outside of Dyad

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -21,7 +21,7 @@ const EditProfile: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-vanta-blue-dark text-white p-8 overflow-y-auto [-webkit-scrollbar:none] [scrollbar-width:none]">
+    <div className="min-h-screen bg-vanta-blue-dark text-white p-8">
       <div className="max-w-4xl mx-auto">
         <h1 className="text-4xl font-bold mb-8 text-center">Edit Profile</h1>
 
